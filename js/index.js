59bb94723148dfa942f6b68764ce18cb09f8e7a9
ragmartgltc <65317907+ragmartgltc@users.noreply.github.com>
2022-10-26 10:43:01 -0500

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+/*Downloaded from https://www.codeseek.co/mahmoudzohdi/slick-slider-syncing-example-WrOJPv */
+
+$('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav',
+    autoplay: true
+});
+$('.slider-nav').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true
+
+});
